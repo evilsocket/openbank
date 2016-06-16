c3c73ca14f992869fc31172b6b80f9b02e62095b
evilsocket <evilsocket@gmail.com>
2016-06-17 00:48:19 +0200

    Fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,7 +30,7 @@ class Kernel extends ConsoleKernel
           ->withoutOverlapping();
 
         $schedule->command('balanceupdate')
-          ->everyThirtyMinutes()
+          ->everyTenMinutes()
           ->withoutOverlapping();
     }
 }
