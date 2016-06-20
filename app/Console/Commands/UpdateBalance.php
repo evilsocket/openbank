c84c1467639a36c5fa452d499fcd28c2921a8028
evilsocket <evilsocket@gmail.com>
2016-06-20 11:19:39 +0200

    Cleaner way of getting key balance.

--- app/Console/Commands/UpdateBalance.php
+++ app/Console/Commands/UpdateBalance.php
@@ -3,38 +3,41 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Log;
 use Cache;
-class UpdateBalance extends Command
+
+class Blockonomics
 {
-    private function doGet( $user, $api_key, $key ){
-      $opts = array('http' =>
-          array(
-              'method'  => 'GET',
-              'header'  => "Content-Type: application/json\r\n" .
-                           "Authorization: Bearer ".$api_key."\r\n" .
-                           "Accept: application/json\r\n",
-          )
-      );
-      $context  = @stream_context_create($opts);
-      $result   = file_get_contents('https://www.blockonomics.co/api/address', false, $context);
-      $response = @json_decode($result, true);
-      return $response;
+  public static function getKeyBalance( $key, $api_key = NULL ){
+    $data = array('addr' => $key);
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => "Content-Type: application/json\r\n" .
+                         "Accept: application/json\r\n" .
+                         ( $api_key ? "Authorization: Bearer $api_key\r\n" : '' ),
+            'content' => @json_encode($data, true)
+        )
+    );
+
+    $context  = @stream_context_create($opts);
+    $result   = @file_get_contents('https://www.blockonomics.co/api/balance', false, $context);
+    $response = @json_decode($result, true);
+    $balance  = 0.0;
+
+    if( !$response || !isset($response['response']) ){
+      return NULL;
     }
 
-    private function doPost( $key ){
-      $data = array('addr' => $key);
-      $opts = array('http' =>
-          array(
-              'method'  => 'POST',
-              'header'  => "Content-Type: application/json\r\n" .
-                           "Accept: application/json\r\n",
-              'content' => @json_encode($data, true)
-          )
-      );
-      $context  = @stream_context_create($opts);
-      $result   = @file_get_contents('https://www.blockonomics.co/api/balance', false, $context);
-      $response = @json_decode($result, true);
-      return $response;
+    $response = $response['response'];
+    foreach( $response as $address ){
+      $balance += $address['confirmed'];
     }
+
+    return $balance / 100000000.0;
+  }
+};
+
+class UpdateBalance extends Command
+{
     /**
      * The name and signature of the console command.
      *
@@ -68,40 +71,13 @@ class UpdateBalance extends Command
         foreach( $keys as $key ){
           Log::info( "  Fetching balance for key '".$key->label."' ( ".$key->value." ) ..." );
 
-          $total = 0;
-
-          // user has api key ?
-          if( $api_key ){
-            $response = $this->doGet( $user, $api_key, $key->value );
-
-            if( !$response || !is_array($response) || !isset($response[0]['balance']) ){
-               Log::error( "! Invalid response json." );
-               continue;
-            }
-
-            foreach($response as $responseData) {
-              if($responseData['address'] == $key->value) {
-                $total = $responseData['balance'];
-              }
-            }
-          }
-          // no api key
-          else {
-            $response = $this->doPost( $key->value );
-            if( !$response || !isset($response['response']) ){
-              Log::error( "! Invalid response json." );
-              continue;
-            }
-
-            $response = $response['response'];
-            foreach( $response as $address ){
-              $total += $address['confirmed'];
-            }
+          $balance = Blockonomics::getKeyBalance( $key->value, $api_key );
+          if( $balance === NULL ){
+            Log::error( "! Invalid response json." );
+            continue;
           }
 
-          $total /= 100000000.0;
-          $key->balance = $total;
-          $key->updated_at = time();
+          $key->balance = $balance;
           $key->save();
         }
 
